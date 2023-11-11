7bf3317fff04882b8212f7ab362f3e42bb9dae18
frobee <frobi.webdev@outlook.com>
2023-11-11 17:09:34 +1030

    write-a-test-which-checks-if-all-ships-are-placed

--- tests/game-loop.test.js
+++ tests/game-loop.test.js
@@ -3,32 +3,38 @@ const ship = require('../src/ship.js');
 const player = require('../src/player.js')
 const gameloop = require('../src/gameloop.js')
 
-let p1;
-let p1Board;
-let p1PlayArea;
-
-let cpu;
-let cpuBoard;
-let cpuPlayArea;
-
-let cruiser;
-let battleship;
+let gameLoop;
 
 beforeEach(() => {
-    p1 = player();
-    p1Board = gameboard();
-    p1PlayArea = p1Board.playArea();
-    
-    cpu = player();
-    cpuBoard = gameboard();
-    cpuPlayArea = cpuBoard.playArea();
-
-    cruiser = ship(3);
-    battleship = ship(4);
+    gameLoop = gameloop();  
 });
 
-describe('gameloop.', () => {
-    it('', () => {
+describe('gameloop', () => {
+    it('Initializes with the correct player and cpu variables', () => {
+        expect(gameLoop.p1Objects).toBeDefined();
+        expect(gameLoop.cpuObjects).toBeDefined()
+    });
+});
+describe('gameloop.placeShipsAtRandom', () => {
+    it('Places a ship of every size at random on the given playArea', () => {
+        const p1PlayArea = gameLoop.p1Objects.p1PlayArea
+        gameLoop.placeShipsAtRandom(p1PlayArea);
 
+        const expectedLengths = [2,3,3,4,5];
+
+        let shipLengths = [];
+        for (let x = 0; x <= 9; x++) {
+            for (let y = 0; y <= 9; y++) {
+                let position = p1PlayArea[x][y]
+                if (position !== undefined) {
+                    shipLengths.push(position.length);
+                };
+            };
+        };
+    
+        const sortedShipLengths = shipLengths.slice().sort();
+        // console.log('shiplengths',sortedShipLengths)
+        const allShipsPlaced = JSON.stringify(expectedLengths) === JSON.stringify(sortedShipLengths);
+        expect(allShipsPlaced).toBe(true);
     });
 });
