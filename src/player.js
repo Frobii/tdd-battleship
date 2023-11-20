a7cbae24397a216039574ff5f00894e0cf5cb73a
frobee <frobi.webdev@outlook.com>
2023-11-20 10:57:01 +1030

    write-a-comment-for-the-loop-check

--- src/player.js
+++ src/player.js
@@ -1,17 +1,15 @@
 const player = () => {
-
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
-    }
+    };
 
     const cpuTurn = (enemyPlayArea, enemyGameBoard) => {
         let x = getRandomInt(10);
         let y = getRandomInt(10);
         let coordinates = [x,y];
 
-        const containsCoords = (array) => array[0] === coordinates[0] && array[1] === coordinates[1];
-        
         // check every coordinate in the hits & misses arrays to ensure a duplicate shot isn't made
+        const containsCoords = (array) => array[0] === coordinates[0] && array[1] === coordinates[1];
         while (enemyGameBoard.hits.some(containsCoords) || enemyGameBoard.misses.some(containsCoords)) {
             x = getRandomInt(10);
             y = getRandomInt(10);
@@ -30,6 +28,6 @@ const player = () => {
         cpuTurn,
         userTurn,
     };
-}
+};
 
 module.exports = player;
