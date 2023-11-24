b1b53651cbb8d771a31566d5f7c8f120c7a01f75
frobee <frobi.webdev@outlook.com>
2023-11-24 20:44:47 +1030

    write-an-untested-version-of-the-paint-functions

--- src/paintBoard.js
+++ src/paintBoard.js
@@ -1,28 +1,61 @@
-const paintBoard = (playerType, playArea, misses, hits) => {
-    if (playerType === 'friendly') {
+const paintBoard = () => {
+    const paintFriendly = (playArea, misses, hits) => {
         const gridContainer = document.querySelector('.friendly-grid');
-    } else if (playerType === 'enemy') {
+        
+        for (let i = 0; i < 10; i++) {
+            const row = document.createElement('div');
+            row.classList.add('grid-row');
+            
+            for (let j = 0; j < 10; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('grid-item');
+                const possibleShip = playArea[i][j];
+                
+                if (misses.includes([i][j])) {
+                    cell.classList.add('miss')
+                } else if (hits.includes([i][j])) {
+                    cell.classList.add('friendly-hit')
+                };
+                
+                // only friendly ships should ever recieve the ship class
+                // enemy ships are never given graphics
+                if (possibleShip !== undefined) {
+                    cell.classList.add('ship');
+                    cell.classList.add(ship.name); 
+                } ;
+                row.appendChild(cell);
+            };
+            gridContainer.appendChild(row);
+        };
+    };
+
+    const paintEnemy = () => {
         const gridContainer = document.getElementById('.enemy-grid')
+        
+        for (let i = 0; i < 10; i++) {
+            const row = document.createElement('div');
+            row.classList.add('grid-row');
+            
+            for (let j = 0; j < 10; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('grid-item');
+                
+                if (misses.includes([i][j])) {
+                    cell.classList.add('miss')
+                } else if (hits.includes([i][j])) {
+                    cell.classList.add('enemy-hit')
+                };
+                
+                row.appendChild(cell);
+            };
+            gridContainer.appendChild(row);
+        };
     };
-    
-    for (let i = 0; i < 10; i++) {
-        const row = document.createElement('div');
-        row.classList.add('grid-row');
 
-        for (let j = 0; j < numCols; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('grid-item');
-            const possibleShip = playArea[i][j]; 
-            if (possibleShip !== undefined) { // check for a ship in this cell
-                cell.classList.add('ship'); // when ships are found give the div a ship class
-                cell.classList.add(ship.name); // and a class for the type of ship 
-            } 
-            row.appendChild(cell);
-        }
-
-        gridContainer.appendChild(row);
+    return {
+        paintFriendly,
+        paintEnemy,
     };
-    
 };
 
 module.exports = paintBoard;
