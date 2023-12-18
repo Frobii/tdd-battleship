462474a2eb6559b7bb5e124a84c63d3dc6b4f095
frobee <frobi.webdev@outlook.com>
2023-12-18 13:43:20 +1030

    design-and-implement-a-win-screen-which-can-reset-the-game

--- src/gameloop.js
+++ src/gameloop.js
@@ -4,61 +4,133 @@ const paintBoard = require('../src/paintBoard.js');
 
 const gameloop = () => {
     const p1 = player();
-    const p1Board = gameboard();
-    const p1PlayArea = p1Board.playArea();
-    const p1Objects = {p1, p1Board, p1PlayArea}
+    let p1Board = gameboard();
+    let p1PlayArea = p1Board.playArea();
 
     const cpu = player();
-    const cpuBoard = gameboard();
-    const cpuPlayArea = cpuBoard.playArea();
-    const cpuObjects = {cpu, cpuBoard, cpuPlayArea};
+    let cpuBoard = gameboard();
+    let cpuPlayArea = cpuBoard.playArea();
 
     const paintDOM = paintBoard();
 
+    const gameOverScreen = document.querySelector('.game-over-screen');
+    const gameOverText = document.querySelector('.game-over-text');
+    
+    const setResetGameEvent = () => {
+        const resetGameButton = document.querySelector('.reset-game');
+        resetGameButton.addEventListener('click', () => {
+            gameOverScreen.style.display = 'none';
+            p1Board = gameboard();
+            p1PlayArea = p1Board.playArea();
+            cpuBoard = gameboard();
+            cpuPlayArea = cpuBoard.playArea();
+            paintDOM.paintFriendly(p1Board, p1PlayArea);
+            paintDOM.paintEnemy(cpuBoard, cpuPlayArea);
+            vsCPU();
+        });
+    };
+    setResetGameEvent();
+
+    // used for testing
+    // p1Board.ships[0].hits = 5;
+    // p1Board.ships[1].hits = 5;
+    // p1Board.ships[2].hits = 5;
+    // p1Board.ships[3].hits = 5;
+    // p1Board.ships[4].hits = 5;
+
+    // used for testing
+    // setResetGameEvent();
+    
+    const checkForP1Win = () => {
+        if (cpuBoard.allSunk()) {
+            gameOverScreen.style.display = 'flex';
+            gameOverText.innerHTML = 'You Won! <br> Would you like to play again?'
+            paintDOM.toggleEnemyClickable();
+            return 'win'
+        };
+    };
+
+    const checkForCPUWin = () => {
+        if (p1Board.allSunk()) {
+            gameOverScreen.style.display = 'flex';
+            gameOverText.innerHTML = 'The CPU Won! <br> Would you like to play again?'
+            return 'win'
+        };
+    };
+
     async function vsCPU() {
-        cpuBoard.placeShipsAtRandom(cpuObjects.cpuPlayArea, cpuObjects.cpuBoard);
-        p1Board.placeShipsAtRandom(p1Objects.p1PlayArea, p1Objects.p1Board);
-        paintDOM.paintFriendly(p1Objects.p1Board, p1Objects.p1PlayArea);
-        let currentCPUHitsLength = cpuObjects.cpuBoard.hits.length;
-        let currentP1HitsLength = p1Objects.p1Board.hits.length;
+        cpuBoard.placeShipsAtRandom(cpuPlayArea, cpuBoard);
+        p1Board.placeShipsAtRandom(p1PlayArea, p1Board);
+
+        // used for testing
+        cpuBoard.ships[0].hits = 5; 
+        cpuBoard.ships[1].hits = 4; 
+        cpuBoard.ships[2].hits = 3; 
+        cpuBoard.ships[3].hits = 3; 
+
+        // used for testing
+        // p1Board.ships[0].hits = 5; 
+        // p1Board.ships[1].hits = 4; 
+        // p1Board.ships[2].hits = 3; 
+        // p1Board.ships[3].hits = 3; 
+
+        paintDOM.paintFriendly(p1Board, p1PlayArea);
+        let currentCPUHitsLength = cpuBoard.hits.length;
+        let currentP1HitsLength = p1Board.hits.length;
         while (!p1Board.allSunk() && !cpuBoard.allSunk()) {
             // Highlight & paint the enemy board
-            paintDOM.paintEnemy(cpuObjects.cpuBoard, cpuObjects.cpuPlayArea);
+            paintDOM.paintEnemy(cpuBoard, cpuPlayArea);
             // Enable clicks on the enemy board so the player can take a turn
             paintDOM.toggleEnemyClickable();
 
-            // Wait for the player's turn
-            do {
-                currentCPUHitsLength = cpuObjects.cpuBoard.hits.length
+            do { // Wait for the player's turn
+                currentCPUHitsLength = cpuBoard.hits.length
                 await p1.waitForPlayerTurn();
+
+
+
+                // used for testing
+                cpuBoard.ships[4].hits = 2; 
+
+
+
+
+                if (checkForP1Win() === 'win') {
+                    return
+                }; // Check if the player made a winning move
             // Give the player an extra turn if they land a shot
-            } while (cpuObjects.cpuBoard.hits.length > currentCPUHitsLength)
+            } while (cpuBoard.hits.length > currentCPUHitsLength)
+
             // Delay the time between turn swaps
-            await new Promise(resolve => setTimeout(resolve, 30));
+            await new Promise(resolve => setTimeout(resolve, 30)); 
             // Disable clicks on the enemy board during the CPU's turn
             paintDOM.toggleEnemyClickable();
             // Highligh the friendly board
             paintDOM.highlightFriendly();
 
-            // Execute CPU's turn
-            do {
-                // Delay the CPU's turn
-                await new Promise(resolve => setTimeout(resolve, 900));
-                currentP1HitsLength = p1Objects.p1Board.hits.length
+            do { // Execute CPU's turn
+                await new Promise(resolve => setTimeout(resolve, 900)); // Delay the CPU's turn
+                currentP1HitsLength = p1Board.hits.length
                 cpu.cpuTurn(p1PlayArea, p1Board);
-                paintDOM.paintFriendly(p1Objects.p1Board, p1Objects.p1PlayArea);
-            } while (p1Objects.p1Board.hits.length > currentP1HitsLength)
-            
+                paintDOM.paintFriendly(p1Board, p1PlayArea);
+
+
+                // used for testing
+                p1Board.ships[4].hits = 2; 
+
+
+
+                checkForCPUWin(); // Check if the CPU made a winning move
+            } while (p1Board.hits.length > currentP1HitsLength)
+
             // Paint the player's board
-            paintDOM.paintFriendly(p1Objects.p1Board, p1Objects.p1PlayArea);
+            paintDOM.paintFriendly(p1Board, p1PlayArea);
             // Delay the time between turn swaps
             await new Promise(resolve => setTimeout(resolve, 30));
         };
     };
    
     return {
-        p1Objects,
-        cpuObjects,
         vsCPU,
     };
 };
